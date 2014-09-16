e1cba8399fb6ab52729524b2a55c65029b505787
LEADR <ian.fennie@gmail.com>
2014-09-16 08:42:53 -0700

    Set up mouse clicks in js

--- js/app.js
+++ js/app.js
@@ -2,8 +2,21 @@ $(document).ready(function() {
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
-  }).mouseleave(function() {
+  })
+  .mouseleave(function() {
     $('.ryu-still').show();
     $('.ryu-ready').hide();
+  })
+  .mousedown(function() {
+    console.log('mousedown');
+    // play hadouken sound
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show();
+    // animate hadouken to the right of the screen
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   });
 });
